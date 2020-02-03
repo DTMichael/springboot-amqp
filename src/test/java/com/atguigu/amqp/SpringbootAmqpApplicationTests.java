8a0e206dc939019a102a6a0840aea14b3943f720
DTMichael <Zhuyi2632@163.com>
2020-02-03 10:39:16 +0800

    初始化项目

---
+++ src/test/java/com/atguigu/amqp/SpringbootAmqpApplicationTests.java
@@ -0,0 +1,13 @@
+package com.atguigu.amqp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringbootAmqpApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
